eb29684ff20929f3f8813d0be71f906e30db4c00
Katherine Jean-Charles <katherinejc103@gmail.com>
2021-12-28 16:18:52 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+// Part one
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`  ;
+}
+
+// part two
+function mondayWork (task = 'go to the office') {
+    return `This Monday, I will ${task}.` ;
+}
+
+// Part three
+function wrapAdjective (wrap = '*') {
+
+    return function characteristic (quality = 'special') {
+        return `You are ${wrap}${quality}${wrap}!`; 
+    }}
+// wrapAdjective ("*", characteristic );
+
+
+
+
+
